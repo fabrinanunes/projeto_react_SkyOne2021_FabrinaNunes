655dfef484aeafb6e8f6d93578447f35196ed0c2
Fabrina Nunes <fabrina.nunes@hotmail.com>
2021-07-16 23:16:25 -0300

    troca de turnos: X e O

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
   renderSquare(i) {
     return <Square
       value={this.state.squares[i]}
@@ -22,17 +21,23 @@ class Board extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { squares: Array(9).fill(null) }
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({ squares: squares })
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   render() {
-    const status = 'Next player: X'
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
     return (
       <div>
         <div className="status">{status}</div>
