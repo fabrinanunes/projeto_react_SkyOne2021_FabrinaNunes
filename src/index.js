de430e05e394daf062a12243b60814b2a742b0aa
Fabrina Nunes <fabrina.nunes@hotmail.com>
2021-07-17 00:31:27 -0300

    desafio concluido

--- src/index.js
+++ src/index.js
@@ -49,12 +49,20 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
@@ -63,15 +71,16 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat([{
-      squares: squares,
+      squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length -1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
     let status
 
@@ -83,9 +92,9 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) =>{
       const desc = move ?
-      `Go to move # ${move}` : 'Go to game start'
+      `Go to move #${move}` : 'Go to game start'
       return(
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
